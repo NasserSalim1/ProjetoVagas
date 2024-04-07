c75efc8565da9024de82aebc66208a652d2c3e12
NasserSalim1 <nasser.salim.beserra@gmail.com>
2024-04-07 18:46:06 -0300

    finalizando o código

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ with open('vagas.pkl', 'rb') as arquivo:
 
 video = cv2.VideoCapture('Video.mp4')
 
+
 while True:
     check,img = video.read()
     imgCinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -17,12 +18,25 @@ while True:
     kernel = np.ones((3,3), np.int8)
     imgDil = cv2.dilate(imgMedian, kernel)
 
+    vagasAbertas = 0
     for x,y,w,h in vagas:
         vaga = imgDil[y:y+h, x:x+w]
         count = cv2.countNonZero(vaga)
         cv2.putText(img, str(count), (x, y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
         cv2.rectangle(img,(x,y), (x+w, y+h), (255,0,0), 2)
+        if count < 950:
+            cv2.rectangle(img,(x,y), (x+w, y+h), (0,255,0), 2)
+            vagasAbertas += 1
+        else:
+            cv2.rectangle(img,(x,y), (x+w, y+h), (0,0,255), 2)
+    
+    cv2.rectangle(img, (90,0), (415,60), (0,255,0), -1)
+    cv2.putText(img,f'LIVRE: {vagasAbertas}/69',(95,45), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 5)
+
+    print(vagasAbertas)
 
     cv2.imshow('video', img)
     cv2.imshow('video Median', imgDil)
     cv2.waitKey(10)
+
+
