653a4b167107cc3f382531f1bca73d606cd890e6
NasserSalim1 <nasser.salim.beserra@gmail.com>
2024-04-13 08:25:24 -0300

    Deletando arquivos desnecessários da pasta

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import pickle
+import pickle 
 import numpy as np
 
 vagas = []
@@ -21,7 +21,7 @@ while True:
     for x,y,w,h in vagas:
         vaga = imgDil[y:y+h, x:x+w]
         count = cv2.countNonZero(vaga)
-        cv2.putText(img, str(count), (x, y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
+        cv2.putText(img, str(count), (x, y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)                          
         cv2.rectangle(img,(x,y), (x+w, y+h), (255,0,0), 2)
         if count < 980:
             cv2.rectangle(img,(x,y), (x+w, y+h), (0,255,0), 2)
@@ -36,5 +36,9 @@ while True:
     cv2.putText(img,f'LIVRE: {porcentagem_vagas_abertas}%',(95,45), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 5)
 
     cv2.imshow('video', img)
-    cv2.imshow('video Median', imgDil)
     cv2.waitKey(10)
+
+
+
+
+
